15e69b6ccdf7fa0b86abcbdca81b648f2e97387f
Fadel <fjachuf@gmail.com>
2023-02-11 13:42:16 -0300

    Discord 0.0.24

---
+++ install.sh
@@ -0,0 +1,26 @@
+echo "Installing DiscordBSD"
+
+mkdir -p ~/.local/bin
+mkdir -p ~/.local/share/discordbsd
+mkdir -p ~/.local/share/applications
+mkdir -p ~/.local/share/icons
+
+chmod +x bin/discord
+
+cp -r RPC ~/.local/share/discordbsd
+cp -r src ~/.local/share/discordbsd/discord
+cp package.json ~/.local/share/discordbsd
+cp bin/* ~/.local/share/bin
+cp share/applications/* ~/.local/share/applications
+cp share/icons/* ~/.local/share/icons
+
+cd ~/.local/share/discordbsd
+npm install
+cd RPC
+npm install
+
+sed -i '' "s/Icon=discicon/Icon=\/home\/$USER\/.local\/share\/icons\/Discord.png/" ~/.local/share/applications/discord.desktop
+
+sed -i '' "s/Exec=executable/Exec=\/home\/$USER\/.local\/bin\/discord/" ~/.local/share/applications/Discord.desktop
+
+echo "Done!"
---
+++ src/index.js
@@ -0,0 +1,85 @@
+const { app, BrowserWindow, Tray, Menu, desktopCapturer } = require("electron");
+const path = require("path");
+const userAgent =
+  "Mozilla/5.0 (X11; FreeBSD x86_64 13982.82.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.157 Safari/537.36";
+
+const fs = require("fs");
+const js = fs.readFileSync(path.join(__dirname, "rpc.js")).toString();
+
+// Disable Menu
+Menu.setApplicationMenu(false);
+
+// Win - Icons Var
+var splash;
+var win = "",
+  appIcon = null,
+  iconpath = path.join(__dirname, "assets/icon.png");
+
+// Create WIndow
+
+function createWindow() {
+  // New Window
+
+  win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    show: false,
+    // frame: false, // Disable appmenu
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    icon: path.join(__dirname, "assets/icon.png"),
+    title: "Discord",
+  });
+
+  // Tray
+  var contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Open Discord",
+      click: function () {
+        win.show();
+      },
+    },
+    {
+      label: "Quit",
+      click: function () {
+        app.isQuiting = true;
+        app.quit();
+      },
+    },
+  ]);
+
+  appIcon = new Tray(iconpath);
+  appIcon.setContextMenu(contextMenu);
+
+  // Close Window
+  win.on("close", function (event) {
+    if (!app.isQuiting) {
+      event.preventDefault();
+      win.hide();
+    }
+
+    return false;
+  });
+
+  //Splash
+  splash = new BrowserWindow({
+    width: 650,
+    height: 450,
+    transparent: true,
+    frame: false,
+    alwaysOnTop: true,
+  });
+  splash.loadFile("boot.html");
+  // Load Discord
+  win.webContents.setUserAgent(userAgent);
+  win.loadURL("https://discord.com/app");
+
+  win.once("ready-to-show", () => {
+    splash.destroy();
+    win.show();
+    win.webContents.executeJavaScript(js);
+  });
+}
+
+app.on("ready", createWindow);
