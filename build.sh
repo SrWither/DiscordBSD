0f218dc476d6926973b55dfdb1d43ef7b79a7af5
Fadel <fjachuf@gmail.com>
2023-07-17 20:24:54 -0300

    DiscordBSD in typescript

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+echo "Need to elevate permissions to pack DiscordBSD. Do you want to execute the command with doas or sudo?"
+select option in "doas" "sudo"; do
+    case $option in
+        doas)
+            discordbsd_perms_command="doas"
+            break
+            ;;
+        sudo)
+            discordbsd_perms_command="sudo"
+            break
+            ;;
+        *)
+            echo "Invalid option. Please select 1 or 2."
+            ;;
+    esac
+done
+
+if [[ -f /usr/ports/Mk/Uses/electron.mk ]]; then
+    echo "electron.mk checked"
+else
+    echo "The file /usr/ports/Mk/Uses/electron.mk does not exist. Please get it from: https://github.com/tagattie/FreeBSD-Electron/tree/master/Mk/Uses"
+fi
+
+# build discord rpc
+git submodule update --init --recursive
+cd RPC
+npm install
+cd ..
+
+# pack discordbsd
+cd freebsd
+$discordbsd_perms_command make makesum
+$discordbsd_perms_command make
+cd ..
+
+# move files
+mkdir -p share/discordbsd
+cp -r RPC share/discordbsd
+cp -r freebsd/work/DiscordBSD-dev-ts/dist/linux-unpacked share/discordbsd/discord
+
+echo "Done!"
---
+++ src/app.ts
@@ -0,0 +1,152 @@
+import { BrowserWindow, Menu, Tray, nativeImage, shell, ipcMain, desktopCapturer } from "electron";
+import { Splash } from "./splash";
+import { setupTitlebar, attachTitlebarToWindow } from "./titlebar/main";
+import { screenshareScript } from "./preload/screenshare"
+import * as path from "path";
+import * as fs from "fs";
+import * as os from "os";
+
+Menu.setApplicationMenu(null);
+setupTitlebar();
+
+export class MainApp {
+  private win: BrowserWindow;
+  private app: Electron.App;
+  private userAgent: string;
+  private icons: { [key: string]: string };
+  private systemTray: Tray;
+  private rpc: string;
+  private splash: Splash;
+
+  /**
+   * Constructs the main application.
+   * @param app The Electron app instance.
+   */
+  constructor(app: Electron.App) {
+    this.app = app;
+    this.userAgent = `Mozilla/5.0 (X11; ${
+      os.type
+    } ${os.arch()}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36`;
+    this.icons = {
+      main: path.join(__dirname, "assets/icons/main.png"),
+      tray: path.join(__dirname, "assets/icons/tray.png"),
+    };
+    this.rpc = fs
+      .readFileSync(path.join(__dirname, "assets/web/js/rpc.js"))
+      .toString();
+    this.splash = new Splash();
+  }
+
+  /**
+   * Creates the tray menu.
+   */
+  private createTrayMenu() {
+    const menu = Menu.buildFromTemplate([
+      {
+        label: "Open Discord",
+        click: () => {
+          this.win.show();
+        },
+      },
+      {
+        label: "Quit",
+        click: () => {
+          this.win.destroy();
+          this.app.quit();
+        },
+      },
+    ]);
+
+    const trayIcon = nativeImage.createFromPath(this.icons.tray);
+    this.systemTray = new Tray(trayIcon);
+    this.systemTray.setContextMenu(menu);
+    this.systemTray.setIgnoreDoubleClickEvents(true);
+  }
+
+  /**
+   * Creates the main browser window.
+   */
+  private createWindow() {
+    this.win = new BrowserWindow({
+      title: "Discord",
+      width: 1280,
+      height: 720,
+      show: false,
+      icon: this.icons.main,
+      titleBarStyle: "hidden",
+      frame: false,
+      webPreferences: {
+        sandbox: false,
+        preload: path.join(__dirname, "preload/preload.js"),
+      },
+    });
+    this.win.webContents.setUserAgent(this.userAgent);
+    this.win.loadURL("https://discord.com/app");
+    attachTitlebarToWindow(this.win);
+  }
+
+  /**
+   * Handles the window close event.
+   */
+  private windowEvents() {
+    // hide window
+    this.win.on("close", (event: Event) => {
+      event.preventDefault();
+      this.win.hide();
+    });
+
+    // Open links in the browser.
+    this.win.webContents.setWindowOpenHandler(
+      ({ url }) => {
+        shell.openExternal(url);
+        return { action: "deny" };
+      }
+    );
+  }
+
+  /**
+   * Handles tray click events.
+   */
+  private trayEvents() {
+    this.systemTray.on("click", () => {
+      if (!this.win.isVisible()) {
+        this.win.show();
+      }
+      this.win.focus();
+    });
+  }
+
+  private ipcEvents() {
+    ipcMain.handle("screenshare-get-sources", (_event, opts) => desktopCapturer.getSources(opts));
+  }
+
+  /**
+   * Starts the main application.
+   */
+  public start() {
+    // Show the splash screen.
+    this.splash.show();
+
+    // Create window
+    this.createWindow();
+
+    // Close splash screen and setup events.
+    this.win.once("ready-to-show", () => {
+      this.splash.close();
+      this.ipcEvents();
+      this.createTrayMenu();
+      this.windowEvents();
+      this.trayEvents();
+      this.win.show();
+      this.win.webContents.executeJavaScript(this.rpc);
+      this.win.webContents.executeJavaScript(screenshareScript);
+    });
+
+    // Handle the activate event to recreate the main window if all windows are closed.
+    this.app.on("activate", () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        this.createWindow();
+      }
+    });
+  }
+}
